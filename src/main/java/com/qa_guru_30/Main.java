f3cd3404b8b244a430459dcd34b85b33731b3ce7
YanaYakovlevaQA <yanna2013yanna@gmail.com>
2025-06-17 00:01:10 +0500

    Update Main.java

--- src/main/java/com/qa_guru_30/Main.java
+++ src/main/java/com/qa_guru_30/Main.java
@@ -2,7 +2,7 @@ package com.qa_guru_30;
 
 public class Main {
 
-    private static String msg = "Hello world!!!";
+    private static String msg = "Hello world";
 
     public static void main(String... args) {
         System.out.println(msg);
