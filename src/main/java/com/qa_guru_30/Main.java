27ea68376ef8383f9328cd88a48b492b08b7ac8e
Yana Yakovleva <yanna2013yanna@gmail.com>
2025-06-16 14:31:43 +0500

    Init commit

---
+++ src/main/java/com/qa_guru_30/Main.java
@@ -0,0 +1,10 @@
+package com.qa_guru_30;
+
+public class Main {
+
+    private static String msg = "Hello qa.guru!";
+
+    public static void main(String... args) {
+        System.out.println(msg);
+    }
+}
